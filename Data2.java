10252b04a24d510ccb1cbfc6f958b0b1f4ff226e
elijah9 <ellucey@vassar.edu>
2015-04-09 11:26:48 -0400

    hopefully this will run soon

--- Data2.java
+++ Data2.java
@@ -1,8 +1,20 @@
 package data2;
 
 public class Data2 {
-
+    
+    public static MultiSet<Integer> set = new Leaf();
+    
     public static void main(String[] args) {
+        set = set.add(1);
+        set = set.add(2);
+        set = set.add(3);
+        set = set.add(4);
+        set = set.add(5);
+        set = set.add(6);
+        set = set.add(7);
+        
+        System.out.println(set.toString());
+        
     
     }
     
