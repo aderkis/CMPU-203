079ea7231de40d2f70752ea41baa806135607f8e
elijah9 <ellucey@vassar.edu>
2015-04-09 11:25:17 -0400

    lots of new goodies, still need to write API

--- MultiSet.java
+++ MultiSet.java
@@ -1,8 +1,19 @@
 package data2;
 
-public abstract class MultiSet<D extends Comparable> {
+public abstract class MultiSet<D extends Comparable> implements Sequenced<D> {
+    
+    public int count;
+    public int height;
+
+    public int multiplicity() {
+        return this.count;
+    }
+    
+    public abstract int multiplicity(D elt);
     
     public abstract int cardinality();
+    
+    public abstract int height();
     
     public abstract boolean isEmptyHuh();
     
@@ -10,7 +21,13 @@ public abstract class MultiSet<D extends Comparable> {
     
     public abstract MultiSet<D> add(D elt);
     
+    public abstract MultiSet<D> add(D elt, int count);
+    
     public abstract MultiSet<D> remove(D elt);
+    
+    public abstract MultiSet<D> remove(D elt, int count);
+    
+    public abstract MultiSet<D> removeAll(D elt);
     
     public abstract MultiSet<D> union(MultiSet<D> set);
     
@@ -21,5 +38,21 @@ public abstract class MultiSet<D extends Comparable> {
     public abstract boolean equal(MultiSet<D> set);
     
     public abstract boolean subset(MultiSet<D> set);
+    
+    public abstract MultiSet<D> balance();
+    
+    public abstract MultiSet<D> rotateLeft();
+    
+    public abstract MultiSet<D> rotateRight();
+    
+    public abstract Sequence<D> seq();
+    
+    public abstract String toString();
+    
+    
+    
+    
+    
+
     
 }
