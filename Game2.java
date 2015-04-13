5b95c1d5d8a932e645f1ac4292f08e797d04be3a
elijah9 <ellucey@vassar.edu>
2015-04-13 12:21:11 -0400

    Update Game2.java

--- Game2.java
+++ Game2.java
@@ -4,32 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import javalib.funworld.*;
 
-
 public class Game2 {
-    
+
     static final int WIDTH = 1000;
     static final int HEIGHT = 750;
-    static boolean isBattle = true;
-    
+    static boolean isBattle = false;
+
     public static void main(String[] args) {
+        Player player = new Player("Jah", 0);
+        Enemy enemy = new Enemy(5, 5, 5);
+        List<Rapper> rappers = new ArrayList<>();
+        Everyone set = new Everyone(player, rappers);
+        set.add(enemy);
         if (isBattle) {
-            Player player = new Player("Jah", 0);
-            Enemy enemy = new Enemy(5, 5, 5, 5);
-            World battle = new Battle(player, enemy);
-            battle.bigBang(WIDTH, HEIGHT, 1);
+            World battle = new Battle(player, enemy, set);
+            battle.bigBang(WIDTH, HEIGHT, 0.1);
         } else {
-            Player player = new Player("Jah", 0);
-            Enemy enemy = new Enemy(5, 5, 5, 5);
-            List<Rapper> rappers = new ArrayList<>();
-            Everyone set = new Everyone(rappers);
             set.add(enemy);
-            World overworld = new Overworld(player, set);
+            World overworld = new Overworld(set);
             overworld.bigBang(WIDTH, HEIGHT, 0.1);
         }
-        
 
     }
-    
+
 
 
 }
