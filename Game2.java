e674cb3aa38310c16ef2e359da25e0dd7121d13a
elijah9 <ellucey@vassar.edu>
2015-04-14 11:40:53 -0400

    Update Game2.java

--- Game2.java
+++ Game2.java
@@ -13,9 +13,8 @@ public class Game2 {
     public static void main(String[] args) {
         Player player = new Player("Jah", 0);
         Enemy enemy = new Enemy(5, 5, 5);
-        List<Rapper> rappers = new ArrayList<>();
-        Everyone set = new Everyone(player, rappers);
-        set.add(enemy);
+        List<Enemy> rappers = new ArrayList<>();
+        Everyone set = new Everyone(player, rappers);        
         if (isBattle) {
             World battle = new Battle(player, enemy, set);
             battle.bigBang(WIDTH, HEIGHT, 0.1);
