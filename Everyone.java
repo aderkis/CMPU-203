8a1f647e4e503bdf2863eb624796688a504b6013
elijah9 <ellucey@vassar.edu>
2015-04-14 11:39:29 -0400

    Update Everyone.java

--- Everyone.java
+++ Everyone.java
@@ -5,35 +5,25 @@ import java.util.List;
 
 public class Everyone {
     
-    public List<Rapper> rappers = new ArrayList();
-    public Rapper player;
+    public List<Enemy> rappers = new ArrayList();
+    public Player player;
     public boolean battle;
     
-    public Everyone(Rapper player, List<Rapper> rappers) {
+    public Everyone(Player player, List<Enemy> rappers) {
         this.player = player;
         this.rappers = rappers;
         this.battle = false;
     }
     
-    public Everyone add(Rapper r) {
+    public Everyone add(Enemy r) {
         Everyone answer = this;
         answer.rappers.add(r);
         return answer;
     }
     
-    public Everyone remove(Rapper r) {
+    public Everyone remove(Enemy r) {
         Everyone answer = this;
         answer.rappers.remove(r);
-        return answer;
-    }
-    
-    public Everyone move(String key) {
-        Everyone answer = this;
-        answer.player = answer.player.move(key);
-        for (int i = 0; i < rappers.size(); i++) {
-            answer.rappers.set(i, rappers.get(i).move(key));
-
-        }
         return answer;
     }
 
